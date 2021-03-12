445a32f6446363c22b913fd9bb32497f163274b4
Alin Ursu <33602125+ursualin7890@users.noreply.github.com>
2021-03-12 13:14:59 +0200

    Diagrama de clase Crawler

---
+++ lab4/GeneralInfo.java
@@ -0,0 +1,21 @@
+public class GeneralInfo {
+
+  private double pollution;
+
+  private double traffic;
+
+  private double noise;
+
+  public double getPollution() {
+  return 0.0;
+  }
+
+  public double getTraffic() {
+  return 0.0;
+  }
+
+  public double getNoise() {
+  return 0.0;
+  }
+
+}
---
+++ lab4/WeatherInfo.java
@@ -0,0 +1,27 @@
+public class WeatherInfo {
+
+  private double temperature;
+
+  private double windSpeed;
+
+  private double humidity;
+
+  private double airPressure;
+
+  public double getTemperature() {
+  return 0.0;
+  }
+
+  public double getWindSpeed() {
+  return 0.0;
+  }
+
+  public double getHumidty() {
+  return 0.0;
+  }
+
+  public double getAirPressure() {
+  return 0.0;
+  }
+
+}
